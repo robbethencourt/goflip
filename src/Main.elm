f86920d33949b832990f5217fadb6664c941c1aa
Robert Bethencourt <robert.bethencourt@gmail.com>
2019-03-29 23:01:48 -0400

    touch events working

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,21 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 import Browser
 import Data.Alphabet as Alphabet
 import Html exposing (Html, div, h1, text)
+import Html.Events.Extra.Touch as Touch
+
+
+
+-- Main
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , subscriptions = \_ -> Sub.none
+        , update = update
+        , view = view
+        }
 
 
 
@@ -10,12 +25,20 @@ import Html exposing (Html, div, h1, text)
 
 
 type alias Model =
-    Page
+    { page : Page
+    , startAt : ( Float, Float )
+    , moveAt : ( Float, Float )
+    , endAt : ( Float, Float )
+    }
 
 
 init : () -> ( Model, Cmd Msg )
 init flags =
-    ( LetterForm Alphabet.A
+    ( { page = LetterForm Alphabet.A
+      , startAt = ( 0, 0 )
+      , moveAt = ( 0, 0 )
+      , endAt = ( 0, 0 )
+      }
     , Cmd.none
     )
 
@@ -31,14 +54,22 @@ type Page
 
 
 type Msg
-    = NoOp
+    = StartAt ( Float, Float )
+    | MoveAt ( Float, Float )
+    | EndAt ( Float, Float )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        StartAt ( x, y ) ->
+            ( { model | startAt = ( x, y ) }, Cmd.none )
+
+        MoveAt ( x, y ) ->
+            ( { model | moveAt = ( x, y ) }, Cmd.none )
+
+        EndAt ( x, y ) ->
+            ( { model | endAt = ( x, y ) }, Cmd.none )
 
 
 
@@ -47,15 +78,21 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    case model of
+    case model.page of
         LetterForm letter ->
             let
                 letterDetails =
                     Alphabet.getLetterDetails letter
             in
-            div []
+            div
+                [ Touch.onStart (StartAt << touchCoordinates)
+                , Touch.onMove (MoveAt << touchCoordinates)
+                , Touch.onEnd (EndAt << touchCoordinates)
+                ]
                 [ h1 []
                     [ text letterDetails.letterText ]
+                , h1 []
+                    [ text <| Debug.toString model ]
                 ]
 
         Drawing letter ->
@@ -74,15 +111,8 @@ view model =
             div [] [ text "this is the nav page" ]
 
 
-
--- Main
-
-
-main : Program () Model Msg
-main =
-    Browser.element
-        { init = init
-        , subscriptions = \_ -> Sub.none
-        , update = update
-        , view = view
-        }
+touchCoordinates : Touch.Event -> ( Float, Float )
+touchCoordinates touchEvent =
+    List.head touchEvent.changedTouches
+        |> Maybe.map .clientPos
+        |> Maybe.withDefault ( 0, 0 )
